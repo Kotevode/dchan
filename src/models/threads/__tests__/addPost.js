4e644ece696baf605012d988edbedf00215818f8
Mark <kotevode@icloud.com>
2018-06-05 23:12:56 +0500

    Add Post tests

---
+++ src/models/threads/__tests__/addPost.js
@@ -0,0 +1,62 @@
+import FeedStore from 'orbit-db-feedstore'
+import { expectSaga } from 'redux-saga-test-plan'
+
+import { actions, types, addPost } from '../'
+
+jest.mock('orbit-db-feedstore')
+
+const hash = "some_hash"
+const address = "some_address"
+const action = actions.addPost({
+  text: "FooBar"
+})
+
+let thread = new FeedStore()
+
+describe("threads#addPost", () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    thread.address = {
+      toString: jest.fn().mockReturnValue(address)
+    }
+  });
+
+  describe("when post success", () => {
+    beforeAll(() => {
+      thread.add.mockReturnValue(
+        Promise.resolve(hash)
+      )
+    });
+
+    it("dispatches success", () => {
+      return expectSaga(addPost, thread, action)
+        .put({
+          type: 'ADD_POST_SUCCESS',
+          payload: {
+            hash,
+            address,
+          }
+        })
+        .run()
+    })
+  })
+
+  describe("when post fails", () => {
+    const error = new Error("WTF")
+
+    beforeAll(() => {
+      thread.add = () => { throw error }
+    });
+
+    it("dispatches error", () => {
+      return expectSaga(addPost, thread, action)
+        .put({
+          type: 'ADD_POST_FAIL',
+          payload: {
+            error
+          }
+        })
+        .run()
+    })
+  })
+})
--- src/models/threads/index.js
+++ src/models/threads/index.js
@@ -1,17 +1,21 @@
-import { apply, put, call } from 'redux-saga/effects'
+import { apply, all, put, call, actionChannel, take } from 'redux-saga/effects'
 import { async, createTypes, actionCreator } from 'redux-action-creator'
 
 // Types
 
 export const types = createTypes([
-  ...async('OPEN_THREAD')
+  ...async('OPEN_THREAD'),
+  ...async('ADD_POST')
 ])
 
 // Actions
 
 export const actions = {
   openThread: actionCreator(types.OPEN_THREAD, 'address'),
-  openThreadSuccess: actionCreator(types.OPEN_THREAD_SUCCESS, 'address')
+  openThreadSuccess: actionCreator(types.OPEN_THREAD_SUCCESS, 'address'),
+  addPost: actionCreator(types.ADD_POST, 'post'),
+  addPostSuccess: actionCreator(types.ADD_POST_SUCCESS, 'hash', 'address'),
+  addPostFail: actionCreator(types.ADD_POST_FAIL, 'error')
 }
 
 // Sagas
@@ -30,8 +34,28 @@ const dbEvents = [
   'replicated'
 ]
 
+export function* addPost(thread, { payload: { post }}) {
+  try {
+    let hash = yield apply(thread, thread.add, [ post ])
+    let address = thread.address.toString()
+    yield put(actions.addPostSuccess(hash, address))
+  } catch (error) {
+    yield put(actions.addPostFail(error))
+  }
+}
+
+export function *watchAddPost(thread) {
+  const requestChan = yield actionChannel(types.ADD_POST)
+  while (true) {
+    let action = yield take(requestChan)
+    yield call(addPost, thread, action)
+  }
+}
+
 export function* serveThread(thread) {
-  yield true
+  yield all([
+    watchAddPost(thread)
+  ])
 }
 
 export function* loadThread(thread) {

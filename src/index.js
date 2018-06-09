3db9f9fcb6384d3c7f61c5d5e06cb87b8c4074c8
Mark <kotevode@icloud.com>
2018-06-09 19:18:37 +0500

    Bootstrap added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 import IPFS from 'ipfs'
 import { Provider } from 'react-redux'
 
